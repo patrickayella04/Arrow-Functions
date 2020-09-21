8bb15f0c503832dde7d96fb2e3985364cccb5341
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-09-22 00:45:18 +0100

    one line functions and returns

---
+++ app.js
@@ -0,0 +1,25 @@
+// Arrow Functions
+//Help us save lines of code and make things look neater. 
+
+// const sayHello = function () {
+//     console.log('Hello');
+// }
+// To turn above function into arrow function, get rid of function key word so you are left with the parenthesis. Then between the last parenthesis and the opening curly brace you want to put an arrow...which is a equals sign and a greater than sign...=>
+// 
+
+// since the function body is only one line above,  We can make this function even more compact by eliminating the code block in the parenthesis all together below.
+
+// One line function does NOT need braces
+// const sayHello = () => console.log('Hello'); 
+
+
+// One line returns
+// const sayHello = () => 'Hello'; // This is how we return something without writing return, but to get the information we will have to console.log the function. 
+
+// Same as above
+// const sayHello = function () {
+//     return 'Hello';
+// }
+
+
+console.log(sayHello());
